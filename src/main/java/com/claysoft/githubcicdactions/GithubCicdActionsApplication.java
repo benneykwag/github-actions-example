3c9cdaafb7d25d33ad670d32df96dbfefb836afc
benneykwag@gmail.com <benneykwag@gmail.com>
2023-11-08 19:56:06 +0900

    dsaf

--- src/main/java/com/claysoft/githubcicdactions/GithubCicdActionsApplication.java
+++ src/main/java/com/claysoft/githubcicdactions/GithubCicdActionsApplication.java
@@ -1,5 +1,6 @@
 package com.claysoft.githubcicdactions;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,12 @@ public class GithubCicdActionsApplication {
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to Spring";
+    }
+    @Value("${app.version:1.0.0}")
+    String version;
+    @GetMapping("/")
+    public String home() {
+        return "Hello CICD! version: " + version;
     }
 
     public static void main(String[] args) {
